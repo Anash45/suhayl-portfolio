6e78405d0cf63dd005aca073717a252cea374713
Syed Anas Bukhari <anas14529@gmail.com>
2024-08-18 18:23:18 +0500

    First.

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Close the dropdown when the cross icon is clicked
+$('#open-menu').on('click', function () {
+  $('#right-menu').fadeIn();
+})
+
+$('#close-menu').on('click', function () {
+  $('#right-menu').fadeOut();
+})
